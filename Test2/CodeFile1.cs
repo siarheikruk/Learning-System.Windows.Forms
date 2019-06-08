f88cbcb84419cea4b827b8c3c1daf248f25bce54
Siarhei Kruk1 <Siarhei_Kruk1@epam.com>
2019-06-08 20:00:56 +0300

    adding some..

--- Test2/CodeFile1.cs
+++ Test2/CodeFile1.cs
@@ -5,6 +5,14 @@ class InputDialogDemo
 {
     static void Main ()
     {
+        string name;
 
+        name = Interaction.InputBox(
+            "What is your name?",
+            "Say Hi!...");
+
+        string txt = "It's pleasure, " + name + "!";
+
+        MessageBox.Show(txt, "we are done.");
     }
 }
